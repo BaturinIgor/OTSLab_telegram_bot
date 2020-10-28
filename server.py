7af4865eee65daa11bdd670609ecbb495d28d9f7
BaturinIgor <baturin29082000@mail.ru>
2020-10-28 12:35:27 +0300

    add python telegram bot

---
+++ server.py
@@ -0,0 +1,13 @@
+import telebot
+
+bot = telebot.TeleBot("1256316393:AAHbVeP4OyhHwzBqCIvlhGEc0y5DXOx9neI")
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, сейчас я расскажу тебе гороскоп на сегодня.")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши Привет")
+    else: bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+bot.polling(none_stop=True, interval=0)
